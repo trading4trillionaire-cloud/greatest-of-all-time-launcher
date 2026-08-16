825c43bee25547352f00bcde21c81f56434548b5
Nihal Thakral <trading4trillionaire@gmail.com>
2026-08-16 07:42:56 +0530

    Bulk upload: app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
     implementation("com.google.android.gms:play-services-ads:23.6.0")
 
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
